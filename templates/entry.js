01de956018072f9c6906540227c7d6c118395765
Henri Bergius <henri.bergius@iki.fi>
2016-06-08 21:34:22 +0200

    Export a require function

--- templates/entry.js
+++ templates/entry.js
@@ -1,1 +1,12 @@
-var noflo = require('noflo');
+var exported = {
+  noflo: require('noflo')
+};
+
+if (window) {
+  window.require = function (moduleName) {
+    if (exported[moduleName]) {
+      return exported[moduleName];
+    }
+    throw new Error('Module ' + moduleName + ' not available');
+  };
+}
